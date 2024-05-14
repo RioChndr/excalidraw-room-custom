e04e99e6d9100560181298a5ec147adcbd5eb3c5
Rio Chandra <me@riochndr.com>
2024-05-14 20:50:05 +0700

    add custom backend excalidraw server

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import debug from "debug";
 import express from "express";
 import http from "http";
-import { Server as SocketIO } from "socket.io";
+import { Socket, Server as SocketIO } from "socket.io";
 
 type UserToFollow = {
-  socketId: string;
-  username: string;
+    socketId: string;
+    username: string;
 };
 type OnUserFollowedPayload = {
-  userToFollow: UserToFollow;
-  action: "FOLLOW" | "UNFOLLOW";
+    userToFollow: UserToFollow;
+    action: "FOLLOW" | "UNFOLLOW";
 };
 
 const serverDebug = debug("server");
@@ -17,137 +17,78 @@ const ioDebug = debug("io");
 const socketDebug = debug("socket");
 
 require("dotenv").config(
-  process.env.NODE_ENV !== "development"
-    ? { path: ".env.production" }
-    : { path: ".env.development" },
+    process.env.NODE_ENV !== "development"
+        ? { path: ".env.production" }
+        : { path: ".env.development" },
 );
 
 const app = express();
 const port =
-  process.env.PORT || (process.env.NODE_ENV !== "development" ? 80 : 3002); // default port to listen
+    process.env.PORT || (process.env.NODE_ENV !== "development" ? 80 : 4012); // default port to listen
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send("Excalidraw collaboration server is up :)");
+    res.send("Excalidraw collaboration server is up :)");
 });
 
 const server = http.createServer(app);
 
 server.listen(port, () => {
-  serverDebug(`listening on port: ${port}`);
+    serverDebug(`listening on port: ${port}`);
 });
 
+interface UserClient {
+    socket: Socket;
+    username: string;
+    role: string;
+}
+
+const listClient = new Map<Socket, UserClient>();
+const listUser = new Map<string, Set<Socket>>();
+
 try {
-  const io = new SocketIO(server, {
-    transports: ["websocket", "polling"],
-    cors: {
-      allowedHeaders: ["Content-Type", "Authorization"],
-      origin: process.env.CORS_ORIGIN || "*",
-      credentials: true,
-    },
-    allowEIO3: true,
-  });
-
-  io.on("connection", (socket) => {
-    ioDebug("connection established!");
-    io.to(`${socket.id}`).emit("init-room");
-    socket.on("join-room", async (roomID) => {
-      socketDebug(`${socket.id} has joined ${roomID}`);
-      await socket.join(roomID);
-      const sockets = await io.in(roomID).fetchSockets();
-      if (sockets.length <= 1) {
-        io.to(`${socket.id}`).emit("first-in-room");
-      } else {
-        socketDebug(`${socket.id} new-user emitted to room ${roomID}`);
-        socket.broadcast.to(roomID).emit("new-user", socket.id);
-      }
-
-      io.in(roomID).emit(
-        "room-user-change",
-        sockets.map((socket) => socket.id),
-      );
+    const io = new SocketIO(server, {
+        transports: ["websocket", "polling"],
+        cors: {
+            allowedHeaders: ["Content-Type", "Authorization"],
+            origin: process.env.CORS_ORIGIN || "*",
+            //   credentials: true,
+        },
+        allowEIO3: true,
     });
 
-    socket.on(
-      "server-broadcast",
-      (roomID: string, encryptedData: ArrayBuffer, iv: Uint8Array) => {
-        socketDebug(`${socket.id} sends update to ${roomID}`);
-        socket.broadcast.to(roomID).emit("client-broadcast", encryptedData, iv);
-      },
-    );
+    io.on("connection", (client) => {
+        ioDebug("connection established!");
+        io.to(`${client.id}`).emit("init-room");
+        client.on("join-room", async (roomID, username, role) => {
+            socketDebug(`${client.id} has joined ${roomID}`);
+            await client.join(roomID);
+            listClient.set(client, { socket: client, username, role });
+            if (!listUser.has(username)) {
+                listUser.set(username, new Set());
+            }
+        });
 
-    socket.on(
-      "server-volatile-broadcast",
-      (roomID: string, encryptedData: ArrayBuffer, iv: Uint8Array) => {
-        socketDebug(`${socket.id} sends volatile update to ${roomID}`);
-        socket.volatile.broadcast
-          .to(roomID)
-          .emit("client-broadcast", encryptedData, iv);
-      },
-    );
+        client.on("on-user-followed", (payload: OnUserFollowedPayload) => {
+            ioDebug("on-user-followed", payload);
+            io.to(payload.userToFollow.socketId).emit("on-user-followed", payload);
+        });
 
-    socket.on("user-follow", async (payload: OnUserFollowedPayload) => {
-      const roomID = `follow@${payload.userToFollow.socketId}`;
+        client.on("broadcast-whiteboard", (roomId: string, data: any) => {
+            ioDebug("broadcast-whiteboard", roomId, data);
+            const username = listClient.get(client)?.username;
+            console.log(username)
+            client.broadcast.to(roomId).emit("client-broadcast-whiteboard", data, username);
+        })
 
-      switch (payload.action) {
-        case "FOLLOW": {
-          await socket.join(roomID);
+        client.on("disconnect", () => {
+            ioDebug("user disconnected");
+        });
+    })
 
-          const sockets = await io.in(roomID).fetchSockets();
-          const followedBy = sockets.map((socket) => socket.id);
-
-          io.to(payload.userToFollow.socketId).emit(
-            "user-follow-room-change",
-            followedBy,
-          );
-
-          break;
-        }
-        case "UNFOLLOW": {
-          await socket.leave(roomID);
-
-          const sockets = await io.in(roomID).fetchSockets();
-          const followedBy = sockets.map((socket) => socket.id);
-
-          io.to(payload.userToFollow.socketId).emit(
-            "user-follow-room-change",
-            followedBy,
-          );
-
-          break;
-        }
-      }
-    });
-
-    socket.on("disconnecting", async () => {
-      socketDebug(`${socket.id} has disconnected`);
-      for (const roomID of Array.from(socket.rooms)) {
-        const otherClients = (await io.in(roomID).fetchSockets()).filter(
-          (_socket) => _socket.id !== socket.id,
-        );
-
-        const isFollowRoom = roomID.startsWith("follow@");
-
-        if (!isFollowRoom && otherClients.length > 0) {
-          socket.broadcast.to(roomID).emit(
-            "room-user-change",
-            otherClients.map((socket) => socket.id),
-          );
-        }
-
-        if (isFollowRoom && otherClients.length === 0) {
-          const socketId = roomID.replace("follow@", "");
-          io.to(socketId).emit("broadcast-unfollow");
-        }
-      }
-    });
-
-    socket.on("disconnect", () => {
-      socket.removeAllListeners();
-      socket.disconnect();
-    });
-  });
 } catch (error) {
-  console.error(error);
+    console.log(error);
+    serverDebug("error starting server", error);
+    process.exit(1);
 }
